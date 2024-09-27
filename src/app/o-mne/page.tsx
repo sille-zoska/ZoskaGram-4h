35a52bdbe2ffdac81ee8ace7724bbfacd3f53141
sille-zoska <sille@zochova.sk>
2024-09-27 10:13:41 +0200

    initial

---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,16 @@
+// src/app/o-mne/page.tsx
+
+
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title: "O mne | ZoškaSnap" };
+
+export default function AboutMe() {
+
+  return (
+
+      <Typography> O mne </Typography>
+
+  );
+}
+
