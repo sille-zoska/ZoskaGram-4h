c826ad30de5c528ab99939ac33329fe0e3c45661
sille-zoska <sille@zochova.sk>
2024-10-25 10:00:58 +0200

    small changes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@
 
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/authOptions";
-import AuthHomeView from "@/sections/AuthHomeView";
+import AuthHomeView from "../../sections/AuthHomeView";
 import NonAuthHomeView from "@/sections/NonAuthHomeView";
 
 export const metadata = { title: "Domov | ZoškaSnap" };
